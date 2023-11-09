160024d425c2ee4df2910a648864ce2acd799341
BLUDUDE02 <stacyscouting@hotmail.com>
2023-11-08 21:57:14 -0500

    Creating UI, Error Handling and Puzzle setup

--- NumberLink AI/Form1.Designer.cs
+++ NumberLink AI/Form1.Designer.cs
@@ -28,20 +28,43 @@
         /// </summary>
         private void InitializeComponent()
         {
+            components = new System.ComponentModel.Container();
             preview1 = new PictureBox();
             preview2 = new PictureBox();
             preview3 = new PictureBox();
             preview4 = new PictureBox();
+            MainFrame = new PictureBox();
+            GeneticAlgorithmGroup = new GroupBox();
+            FitIn = new MaskedTextBox();
+            FitLbl = new Label();
+            MutIn = new MaskedTextBox();
+            MutLbl = new Label();
+            GenIn = new MaskedTextBox();
+            PopIn = new MaskedTextBox();
+            GenLbl = new Label();
+            PopLbl = new Label();
+            ResultsBox = new GroupBox();
+            ConnectionsLbl = new Label();
+            AverageConnectionsHeader = new Label();
+            BestScore = new Label();
+            BestName = new Label();
+            label4 = new Label();
+            StartButton = new Button();
+            ErrorProvider = new ErrorProvider(components);
             ((System.ComponentModel.ISupportInitialize)preview1).BeginInit();
             ((System.ComponentModel.ISupportInitialize)preview2).BeginInit();
             ((System.ComponentModel.ISupportInitialize)preview3).BeginInit();
             ((System.ComponentModel.ISupportInitialize)preview4).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)MainFrame).BeginInit();
+            GeneticAlgorithmGroup.SuspendLayout();
+            ResultsBox.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)ErrorProvider).BeginInit();
             SuspendLayout();
             // 
             // preview1
             // 
             preview1.BackColor = Color.Black;
-            preview1.Location = new Point(688, 12);
+            preview1.Location = new Point(436, 12);
             preview1.Name = "preview1";
             preview1.Size = new Size(100, 100);
             preview1.TabIndex = 0;
@@ -51,7 +74,7 @@
             // preview2
             // 
             preview2.BackColor = Color.Black;
-            preview2.Location = new Point(688, 118);
+            preview2.Location = new Point(436, 118);
             preview2.Name = "preview2";
             preview2.Size = new Size(100, 100);
             preview2.TabIndex = 1;
@@ -61,7 +84,7 @@
             // preview3
             // 
             preview3.BackColor = Color.Black;
-            preview3.Location = new Point(688, 224);
+            preview3.Location = new Point(436, 224);
             preview3.Name = "preview3";
             preview3.Size = new Size(100, 100);
             preview3.TabIndex = 2;
@@ -71,18 +94,209 @@
             // preview4
             // 
             preview4.BackColor = Color.Black;
-            preview4.Location = new Point(688, 330);
+            preview4.Location = new Point(436, 330);
             preview4.Name = "preview4";
             preview4.Size = new Size(100, 100);
             preview4.TabIndex = 3;
             preview4.TabStop = false;
             preview4.Click += preview4_Click;
             // 
+            // MainFrame
+            // 
+            MainFrame.BackColor = Color.Black;
+            MainFrame.Location = new Point(12, 12);
+            MainFrame.Name = "MainFrame";
+            MainFrame.Size = new Size(418, 418);
+            MainFrame.TabIndex = 4;
+            MainFrame.TabStop = false;
+            // 
+            // GeneticAlgorithmGroup
+            // 
+            GeneticAlgorithmGroup.BackColor = Color.Linen;
+            GeneticAlgorithmGroup.Controls.Add(FitIn);
+            GeneticAlgorithmGroup.Controls.Add(FitLbl);
+            GeneticAlgorithmGroup.Controls.Add(MutIn);
+            GeneticAlgorithmGroup.Controls.Add(MutLbl);
+            GeneticAlgorithmGroup.Controls.Add(GenIn);
+            GeneticAlgorithmGroup.Controls.Add(PopIn);
+            GeneticAlgorithmGroup.Controls.Add(GenLbl);
+            GeneticAlgorithmGroup.Controls.Add(PopLbl);
+            GeneticAlgorithmGroup.ForeColor = Color.SaddleBrown;
+            GeneticAlgorithmGroup.Location = new Point(542, 12);
+            GeneticAlgorithmGroup.Name = "GeneticAlgorithmGroup";
+            GeneticAlgorithmGroup.Size = new Size(150, 146);
+            GeneticAlgorithmGroup.TabIndex = 5;
+            GeneticAlgorithmGroup.TabStop = false;
+            GeneticAlgorithmGroup.Text = "Parameters";
+            // 
+            // FitIn
+            // 
+            FitIn.BackColor = Color.Snow;
+            FitIn.ForeColor = Color.SaddleBrown;
+            FitIn.Location = new Point(84, 114);
+            FitIn.Mask = "000";
+            FitIn.Name = "FitIn";
+            FitIn.RightToLeft = RightToLeft.Yes;
+            FitIn.Size = new Size(40, 23);
+            FitIn.TabIndex = 7;
+            // 
+            // FitLbl
+            // 
+            FitLbl.AutoSize = true;
+            FitLbl.Location = new Point(6, 119);
+            FitLbl.Name = "FitLbl";
+            FitLbl.Size = new Size(43, 15);
+            FitLbl.TabIndex = 6;
+            FitLbl.Text = "Fitness";
+            // 
+            // MutIn
+            // 
+            MutIn.BackColor = Color.Snow;
+            MutIn.ForeColor = Color.SaddleBrown;
+            MutIn.Location = new Point(84, 85);
+            MutIn.Mask = "000";
+            MutIn.Name = "MutIn";
+            MutIn.RightToLeft = RightToLeft.Yes;
+            MutIn.Size = new Size(40, 23);
+            MutIn.TabIndex = 5;
+            // 
+            // MutLbl
+            // 
+            MutLbl.AutoSize = true;
+            MutLbl.Location = new Point(6, 90);
+            MutLbl.Name = "MutLbl";
+            MutLbl.Size = new Size(69, 15);
+            MutLbl.TabIndex = 4;
+            MutLbl.Text = "Mutation %";
+            // 
+            // GenIn
+            // 
+            GenIn.BackColor = Color.Snow;
+            GenIn.ForeColor = Color.SaddleBrown;
+            GenIn.Location = new Point(84, 56);
+            GenIn.Mask = "000";
+            GenIn.Name = "GenIn";
+            GenIn.RightToLeft = RightToLeft.Yes;
+            GenIn.Size = new Size(40, 23);
+            GenIn.TabIndex = 3;
+            // 
+            // PopIn
+            // 
+            PopIn.BackColor = Color.Snow;
+            PopIn.ForeColor = Color.SaddleBrown;
+            PopIn.Location = new Point(84, 27);
+            PopIn.Mask = "000";
+            PopIn.Name = "PopIn";
+            PopIn.RightToLeft = RightToLeft.Yes;
+            PopIn.Size = new Size(40, 23);
+            PopIn.TabIndex = 2;
+            // 
+            // GenLbl
+            // 
+            GenLbl.AutoSize = true;
+            GenLbl.Location = new Point(6, 60);
+            GenLbl.Name = "GenLbl";
+            GenLbl.Size = new Size(70, 15);
+            GenLbl.TabIndex = 1;
+            GenLbl.Text = "Generations";
+            // 
+            // PopLbl
+            // 
+            PopLbl.AutoSize = true;
+            PopLbl.Location = new Point(6, 30);
+            PopLbl.Name = "PopLbl";
+            PopLbl.Size = new Size(65, 15);
+            PopLbl.TabIndex = 0;
+            PopLbl.Text = "Population";
+            // 
+            // ResultsBox
+            // 
+            ResultsBox.BackColor = Color.Linen;
+            ResultsBox.Controls.Add(ConnectionsLbl);
+            ResultsBox.Controls.Add(AverageConnectionsHeader);
+            ResultsBox.Controls.Add(BestScore);
+            ResultsBox.Controls.Add(BestName);
+            ResultsBox.Controls.Add(label4);
+            ResultsBox.ForeColor = Color.SaddleBrown;
+            ResultsBox.Location = new Point(542, 164);
+            ResultsBox.Name = "ResultsBox";
+            ResultsBox.Size = new Size(150, 148);
+            ResultsBox.TabIndex = 8;
+            ResultsBox.TabStop = false;
+            ResultsBox.Text = "Results Overview";
+            // 
+            // ConnectionsLbl
+            // 
+            ConnectionsLbl.AutoSize = true;
+            ConnectionsLbl.Location = new Point(6, 115);
+            ConnectionsLbl.Name = "ConnectionsLbl";
+            ConnectionsLbl.Size = new Size(74, 15);
+            ConnectionsLbl.TabIndex = 12;
+            ConnectionsLbl.Text = "Connections";
+            // 
+            // AverageConnectionsHeader
+            // 
+            AverageConnectionsHeader.AutoSize = true;
+            AverageConnectionsHeader.Location = new Point(6, 100);
+            AverageConnectionsHeader.Name = "AverageConnectionsHeader";
+            AverageConnectionsHeader.Size = new Size(120, 15);
+            AverageConnectionsHeader.TabIndex = 11;
+            AverageConnectionsHeader.Text = "Average Connections";
+            // 
+            // BestScore
+            // 
+            BestScore.AutoSize = true;
+            BestScore.Location = new Point(6, 60);
+            BestScore.Name = "BestScore";
+            BestScore.Size = new Size(77, 15);
+            BestScore.TabIndex = 10;
+            BestScore.Text = "Connections:";
+            // 
+            // BestName
+            // 
+            BestName.AutoSize = true;
+            BestName.Location = new Point(6, 45);
+            BestName.Name = "BestName";
+            BestName.Size = new Size(45, 15);
+            BestName.TabIndex = 9;
+            BestName.Text = "Name: ";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(6, 30);
+            label4.Name = "label4";
+            label4.Size = new Size(92, 15);
+            label4.TabIndex = 8;
+            label4.Text = "Most Successful";
+            // 
+            // StartButton
+            // 
+            StartButton.BackColor = Color.Snow;
+            StartButton.ForeColor = Color.SaddleBrown;
+            StartButton.Location = new Point(542, 318);
+            StartButton.Name = "StartButton";
+            StartButton.Size = new Size(150, 111);
+            StartButton.TabIndex = 9;
+            StartButton.Text = "Start!";
+            StartButton.UseVisualStyleBackColor = false;
+            StartButton.Click += StartButton_Click;
+            // 
+            // ErrorProvider
+            // 
+            ErrorProvider.BlinkStyle = ErrorBlinkStyle.NeverBlink;
+            ErrorProvider.ContainerControl = this;
+            // 
             // Window
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            BackColor = Color.PeachPuff;
+            ClientSize = new Size(704, 441);
+            Controls.Add(StartButton);
+            Controls.Add(ResultsBox);
+            Controls.Add(GeneticAlgorithmGroup);
+            Controls.Add(MainFrame);
             Controls.Add(preview4);
             Controls.Add(preview3);
             Controls.Add(preview2);
@@ -93,6 +307,12 @@
             ((System.ComponentModel.ISupportInitialize)preview2).EndInit();
             ((System.ComponentModel.ISupportInitialize)preview3).EndInit();
             ((System.ComponentModel.ISupportInitialize)preview4).EndInit();
+            ((System.ComponentModel.ISupportInitialize)MainFrame).EndInit();
+            GeneticAlgorithmGroup.ResumeLayout(false);
+            GeneticAlgorithmGroup.PerformLayout();
+            ResultsBox.ResumeLayout(false);
+            ResultsBox.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)ErrorProvider).EndInit();
             ResumeLayout(false);
         }
 
@@ -102,5 +322,23 @@
         private PictureBox preview2;
         private PictureBox preview3;
         private PictureBox preview4;
+        private PictureBox MainFrame;
+        private GroupBox GeneticAlgorithmGroup;
+        private Label GenLbl;
+        private Label PopLbl;
+        private MaskedTextBox FitIn;
+        private Label FitLbl;
+        private MaskedTextBox MutIn;
+        private Label MutLbl;
+        private MaskedTextBox GenIn;
+        private MaskedTextBox PopIn;
+        private GroupBox ResultsBox;
+        private Label label4;
+        private Label AverageConnectionsHeader;
+        private Label BestScore;
+        private Label BestName;
+        private Label ConnectionsLbl;
+        private Button StartButton;
+        private ErrorProvider ErrorProvider;
     }
 }
--- NumberLink AI/Form1.cs
+++ NumberLink AI/Form1.cs
@@ -1,14 +1,66 @@
+using System.Diagnostics.Eventing.Reader;
+
 namespace NumberLink_AI
 {
     public partial class Window : Form
     {
+        public int puzzle = 1;
+        private int population = 0;
+        private int generations = 0;
+        private int mutationRate = 0;
+        private int fitness = 0;
+
+        private static Color[] colors = {Color.Red, Color.Orange,
+            Color.Yellow, Color.Green, Color.Blue, Color.Cyan, Color.Magenta};
+
+        #region Puzzle Declarations
+        static int[,] puzzle1 = { { 5, 0, 0, 0, 3 },
+                           { 0, 0, 0, 0, 0 },
+                           { 0, 1, 4, 0, 0 },
+                           { 0, 0, 5, 0, 0 },
+                           { 4, 1, 3, 0, 0 } };
+
+        static int[,] puzzle2 = { { 0, 0, 0, 0, 0 },
+                           { 2, 1, 0, 1, 2 },
+                           { 3, 0, 0, 0, 0 },
+                           { 0, 5, 0, 4, 0 },
+                           { 3, 4, 0, 5, 0 } };
+
+        static int[,] puzzle3 = { { 0, 0, 0, 0, 0, 0 },
+                           { 5, 1, 3, 2, 0, 0 },
+                           { 1, 0, 0, 4, 0, 0 },
+                           { 4, 0, 0, 0, 0, 5 },
+                           { 0, 3, 0, 0, 2, 6 },
+                           { 0, 0, 0, 6, 0, 0 } };
+
+        static int[,] puzzle4 = { { 0, 0, 0, 0, 2, 7, 2, 0 },
+                           { 0, 0, 0, 0, 1, 0, 6, 0 },
+                           { 0, 0, 4, 0, 0, 0, 0, 0 },
+                           { 0, 0, 0, 0, 0, 7, 6, 0 },
+                           { 0, 0, 4, 0, 0, 0, 0, 0 },
+                           { 0, 0, 0, 0, 0, 0, 0, 0 },
+                           { 0, 5, 3, 1, 0, 0, 0, 0 },
+                           { 0, 0, 0, 0, 0, 0, 0, 0 }};
+
+        new readonly Dictionary<int, int[,]> puzzles = new Dictionary<int, int[,]>
+        {
+            {1, puzzle1},
+            {2, puzzle2},
+            {3, puzzle3},
+            {4, puzzle4}
+        };
+        #endregion
+
+
         public Window()
         {
             InitializeComponent();
+            InitializePuzzles();
         }
 
-        #region Puzzle Selection
+        #region UI
 
+        #region Puzzle Selection
         /// <summary>
         /// Handles Selecting Puzzle 1
         /// </summary>
@@ -16,9 +68,9 @@ namespace NumberLink_AI
         /// <param name="e"></param>
         private void preview1_Click(object sender, EventArgs e)
         {
-
+            puzzle = 1;
+            InitializePuzzles();
         }
-
         /// <summary>
         /// Handles Selecting Puzzle 2
         /// </summary>
@@ -26,9 +78,9 @@ namespace NumberLink_AI
         /// <param name="e"></param>
         private void preview2_Click(object sender, EventArgs e)
         {
-
+            puzzle = 2;
+            InitializePuzzles();
         }
-
         /// <summary>
         /// Handles Selecting Puzzle 3
         /// </summary>
@@ -36,9 +88,9 @@ namespace NumberLink_AI
         /// <param name="e"></param>
         private void preview3_Click(object sender, EventArgs e)
         {
-
+            puzzle = 3;
+            InitializePuzzles();
         }
-
         /// <summary>
         /// Handle Selecting Puzzle 4
         /// </summary>
@@ -46,8 +98,219 @@ namespace NumberLink_AI
         /// <param name="e"></param>
         private void preview4_Click(object sender, EventArgs e)
         {
-
+            puzzle = 4;
+            InitializePuzzles();
         }
+        #endregion
+
+        /// <summary>
+        /// Handle Game Start.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void StartButton_Click(object sender, EventArgs e)
+        {
+            if (ValidateInputs())
+            {
+                ErrorProvider.Clear();
+                population = int.Parse(PopIn.Text);
+                generations = int.Parse(GenIn.Text);
+                mutationRate = int.Parse(MutIn.Text);
+                fitness = int.Parse(FitIn.Text);
+
+                MessageBox.Show("Selected Puzzle: " + puzzle +
+                    "\nPopulation Size: " + population +
+                    "\nGeneration Size: " + generations +
+                    "\nMutation Rate: " + mutationRate +
+                    "\nFitness Size: " + fitness);
+            }
+        }
+
+        private void InitializePuzzles()
+        {
+            DrawPuzzle(preview1, puzzles[1]);
+            DrawPuzzle(preview2, puzzles[2]);
+            DrawPuzzle(preview3, puzzles[3]);
+            DrawPuzzle(preview4, puzzles[4]);
+        }
+
+        /// <summary>
+        /// Draw a numberlink grid in a given picturebox, and a solution, if given.
+        /// </summary>
+        /// <param name="image"></param>
+        /// <param name="grid"></param>
+        /// <param name="solution"></param>
+        private void DrawPuzzle(PictureBox image, int[,] grid, int[,]? solution = null)
+        {
+            int hgt = grid.GetUpperBound(0) + 1;
+            int wid = grid.GetUpperBound(1) + 1;
+
+            int CellWid = image.Width / (wid + 2);
+            int CellHgt = image.Height / (hgt + 2);
+
+            if (CellWid > CellHgt) CellWid = CellHgt;
+            else CellHgt = CellWid;
+            int Xmin = (image.Width - wid * CellWid) / 2;
+            int Ymin = (image.Height - hgt * CellHgt) / 2;
+
+            Bitmap bm = new Bitmap(
+                image.Width,
+                image.Height);
+            using (Graphics gr = Graphics.FromImage(bm))
+            {
+                gr.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.NearestNeighbor;
+                Rectangle rect = new Rectangle(2, 2, image.Width - 4, image.Height - 4);
+                SolidBrush brush = new SolidBrush(Color.Black);
+                Pen pen = new Pen(Color.Brown, 6);
+
+                //Draw grid nodes.
+                gr.FillRectangle(brush, rect);
+                for (int y = 0; y < hgt; y++)
+                {
+                    int ycoord = y * CellHgt + Ymin;
+                    for (int x = 0; x < wid; x++)
+                    {
+                        int xcoord = x * CellWid + Xmin;
+                        rect = new Rectangle(xcoord - 1, ycoord - 1, CellWid - 2, CellHgt - 2);
+                        if (grid[x, y] != 0)
+                        {
+                            brush = new SolidBrush(colors[grid[x, y] - 1]);
+                            gr.FillEllipse(brush, rect);
+                        }
+                    }
+                }
+
+                //Draw solution (NOT IMPLEMENTED)
+
+                //Highlight selected box
+                rect = new Rectangle(0, 0, image.Width, image.Height);
+                switch (image.Name)
+                {
+                    case "preview1":
+                        if (puzzle == 1)
+                        {
+                            gr.DrawRectangle(pen, rect);
+                        }
+                        break;
+                    case "preview2":
+                        if (puzzle == 2)
+                        {
+                            gr.DrawRectangle(pen, rect);
+                        }
+                        break;
+                    case "preview3":
+                        if (puzzle == 3)
+                        {
+
+                            gr.DrawRectangle(pen, rect);
+                        }
+                        break;
+                    case "preview4":
+                        if (puzzle == 4)
+                        {
+                            gr.DrawRectangle(pen, rect);
+                        }
+                        break;
+                    default:
+                        break;
+                }
+
+            }
+            image.Image = bm;
+        }
+
+        #region Error Handling
+        /// <summary>
+        /// Validate all possible UI errors
+        /// </summary>
+        /// <returns></returns>
+        private bool ValidateInputs()
+        {
+            //this boolean lets us still fail, but highlight all errors.
+            bool test = true;
+
+            ErrorProvider.Clear();
+
+            //If selected puzzle is invalid
+            //(should be impossible, but hey ho).
+            if (puzzle < 0 || puzzle > 4)
+            {
+                MessageBox.Show("FATAL ERROR :(");
+                Application.Exit();
+            }
+
+            //If any text boxes are empty.
+            if (PopIn.Text == String.Empty)
+            {
+                ErrorProvider.SetError(PopIn, "Please enter a value here.");
+                test = false;
+            }
+
+            if (GenIn.Text == String.Empty)
+            {
+                ErrorProvider.SetError(GenIn, "Please enter a value here.");
+                test = false;
+            }
+
+            if (MutIn.Text == String.Empty)
+            {
+                ErrorProvider.SetError(MutIn, "Please enter a value here.");
+                test = false;
+            }
+
+            if (FitIn.Text == String.Empty)
+            {
+                ErrorProvider.SetError(FitIn, "Please enter a value here.");
+                test = false;
+            }
+
+            //If the Population Box is < 2
+            if (PopIn.Text != String.Empty && int.Parse(PopIn.Text) < 2)
+            {
+                ErrorProvider.SetError(PopIn, "Please enter value greater than 1.");
+                test = false;
+            }
+
+            //If the Generation Box is < 1
+            if (GenIn.Text != String.Empty && int.Parse(GenIn.Text) < 1)
+            {
+                ErrorProvider.SetError(GenIn, "Please enter value greater than 0.");
+                test = false;
+            }
+
+            //If the Mutation Box is < 1
+            if (MutIn.Text != String.Empty && int.Parse(MutIn.Text) < 1)
+            {
+                ErrorProvider.SetError(MutIn, "Please enter value greater than 0.");
+                test = false;
+            }
+
+            //If the Mutation Box is > 100
+            if (MutIn.Text != String.Empty && int.Parse(MutIn.Text) > 100)
+            {
+                ErrorProvider.SetError(MutIn, "Please enter value less than 100.");
+                test = false;
+            }
+
+            //If the Fitness Box is < 2
+            if (FitIn.Text != String.Empty && int.Parse(FitIn.Text) < 2)
+            {
+                ErrorProvider.SetError(FitIn, "Please enter value greater than 1.");
+                test = false;
+            }
+
+            //If the Fitness Box is > Population Size!
+            if ((FitIn.Text != String.Empty && PopIn.Text != String.Empty) &&
+                int.Parse(FitIn.Text) > int.Parse(PopIn.Text) + 1)
+            {
+                ErrorProvider.SetError(FitIn, "Please enter value less than " +
+                    (int.Parse(PopIn.Text) + 1) + ".");
+                test = false;
+            }
+
+            return test;
+        }
+        #endregion
 
         #endregion
     }
